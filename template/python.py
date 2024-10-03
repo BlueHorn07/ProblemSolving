1677ae68508568bc0969c170f98622f2a79529b6
BlueHorn07 <bluehornblues@gmail.com>
2024-10-03 23:09:52 +0900

    update python template

--- template/python.py
+++ template/python.py
@@ -1,4 +1,6 @@
 import sys
 
-input = sys.stdin.readline().rstrip
+input = sys.stdin.readline
+# input = sys.stdin.readline().rstrip # Remove newline character
+input_nums = lambda: list(map(int, input().split()))
 
